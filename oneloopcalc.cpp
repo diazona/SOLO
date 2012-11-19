164a29dd11ce004ce3fc0513fb4a3c4f27110689
David Zaslavsky <diazona@ellipsix.net>
2012-11-19 15:57:07 -0500

    Split out calculation of alphasbar into a separate class

--- oneloopcalc.cpp
+++ oneloopcalc.cpp
@@ -46,6 +46,7 @@ void eprint(char* s) { cerr << s << endl; }
 
 class IntegrationContext;
 class GluonDistribution;
+class Coupling;
 class HardFactor;
 
 class Context {
@@ -65,17 +66,17 @@ public:
     double Y;
     double Q02x0lambda;
     double tau;
-    double alphasbar_fixed;
     
     GluonDistribution* gdist;
+    Coupling* cpl;
 
     // NOTE: these contain state that should be associated with IntegrationContext.
     // So don't use one Context with more than one IntegrationContext at once.
     c_mstwpdf* pdf_object;
     DSSpiNLO* ff_object;
 
-    Context(double x0, double A, double c, double lambda, double mu2, double Nc, double Nf, double CF, double TR, double Sperp, double pT2, double sqs, double Y, double alphasbar_fixed, GluonDistribution* gdist, const char* pdf_filename, const char* ff_filename) :
-     x0(x0), A(A), c(c), lambda(lambda), mu2(mu2), Nc(Nc), Nf(Nf), CF(CF), TR(TR), Sperp(Sperp), pT2(pT2), sqs(sqs), Y(Y), alphasbar_fixed(alphasbar_fixed), gdist(gdist) {
+    Context(double x0, double A, double c, double lambda, double mu2, double Nc, double Nf, double CF, double TR, double Sperp, double pT2, double sqs, double Y, GluonDistribution* gdist, Coupling* cpl, const char* pdf_filename, const char* ff_filename) :
+     x0(x0), A(A), c(c), lambda(lambda), mu2(mu2), Nc(Nc), Nf(Nf), CF(CF), TR(TR), Sperp(Sperp), pT2(pT2), sqs(sqs), Y(Y), gdist(gdist), cpl(cpl) {
          recalculate();
          pdf_object = new c_mstwpdf(pdf_filename);
          ff_object = new DSSpiNLO(ff_filename);
@@ -94,9 +95,32 @@ public:
          Q02x0lambda = c * pow(A, 1.0d/3.0d) * pow(x0, lambda);
          tau = sqrt(pT2)/sqs*exp(Y);
     }
-    
-    virtual double alphasbar(double kT2) {
-        return alphasbar_fixed;
+};
+
+class Coupling {
+public:
+    virtual double alphasbar(double kT2) = 0;
+};
+
+class FixedCoupling : public Coupling {
+private:
+    double value;
+public:
+    FixedCoupling(double alphasbar) : value(alphasbar) {}
+    double alphasbar(double kT2) {
+        return value;
+    }
+};
+
+class LORunningCoupling : public Coupling {
+private:
+    double log_LambdaQCD;
+    double inverse_beta_2pi;
+    double regulator; // position of the Landau pole
+public:
+    LORunningCoupling(double LambdaQCD, double beta, double regulator) : log_LambdaQCD(log(LambdaQCD)), inverse_beta_2pi(0.5 / (M_PI * beta)), regulator(regulator) {}
+    double alphasbar(double kT2) {
+        return inverse_beta_2pi / (log(kT2 + regulator) - log_LambdaQCD);
     }
 };
 
@@ -202,7 +226,7 @@ void IntegrationContext::update(double z, double y, double xx, double xy, double
     this->kT = sqrt(this->kT2);
     this->xg = kT / ctx->sqs * exp(-ctx->Y);
     this->Qs2 = ctx->Q02x0lambda / pow(this->xg, ctx->lambda); // Q_0^2 (x_0 / x)^lambda
-    this->alphasbar = ctx->alphasbar(this->kT2);
+    this->alphasbar = ctx->cpl->alphasbar(this->kT2);
 
     // Calculate the new gluon distribution values
     // this has to be done after kinematics are updated
@@ -1039,6 +1063,7 @@ double calculateHardFactor(Context* ctx, size_t hflen, HardFactor** hflist) {
 int main(int argc, char** argv) {
     bool separate = false;
     GluonDistribution* gdist = new GBWGluonDistribution();
+    Coupling* cpl = new FixedCoupling(0.2 / (2*M_PI));
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--miser")==0) {
             integration_strategy = MC_MISER;
@@ -1068,8 +1093,8 @@ int main(int argc, char** argv) {
       1.0,      // pT2 (dummy value)
       200,      // sqs
       3.2,      // Y
-      0.2 / (2*M_PI), // alphasbar
       gdist,
+      cpl,
       "mstw2008nlo.00.dat", "PINLO.DAT");
 
     double pT[] = {0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4};
