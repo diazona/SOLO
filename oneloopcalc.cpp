016bada02286bebf76114725ee00654d8b766654
David Zaslavsky <diazona@ellipsix.net>
2012-11-19 23:21:39 -0500

    Check consistency and correctness of qq terms; introduce some abbreviations in LaTeX doc

--- oneloopcalc.cpp
+++ oneloopcalc.cpp
@@ -332,7 +332,7 @@ public:
         return dipole;
     }
     void Fd(IntegrationContext* ictx, double* real, double* imag) {
-        double amplitude = 1/(4*M_PI*M_PI) * ictx->qqfactor / ictx->z2 * ictx->S2r;
+        double amplitude = 0.25*M_1_PI*M_1_PI * ictx->ctx->Sperp / ictx->z2 * ictx->qqfactor * ictx->S2r;
         double phase = -ictx->kT * ictx->rx; // take angle of k_perp to be 0
         *real = amplitude * cos(phase);
         *imag = amplitude * sin(phase);
@@ -345,34 +345,33 @@ public:
         return dipole;
     }
     void Fs(IntegrationContext* ictx, double* real, double* imag) {
-        double amplitude = 1/(4*M_PI*M_PI) * ictx->alphasbar * ictx->qqfactor / ictx->z2 * ictx->S2r *
-          ictx->ctx->CF * (1 + ictx->xi2) * (-2*M_EULER + log(4) - log(ictx->r2 * ictx->ctx->mu2));
+        double amplitude = 0.25*M_1_PI*M_1_PI * ictx->alphasbar * ictx->ctx->CF * ictx->ctx->Sperp / ictx->z2 * ictx->qqfactor * ictx->S2r *
+          (1 + ictx->xi2) * (-2*M_EULER + log(4) - log(ictx->r2 * ictx->ctx->mu2));
         double phase1 = -ictx->kT * ictx->rx;
         double phase2 = -ictx->kT * ictx->rx / ictx->xi;
         *real = amplitude * (cos(phase1) + cos(phase2) / ictx->xi2);
         *imag = amplitude * (sin(phase1) + sin(phase2) / ictx->xi2);
     }
     void Fd(IntegrationContext* ictx, double* real, double* imag) {
-        double amplitude = 1/(4*M_PI*M_PI) * ictx->alphasbar * ictx->qqfactor / ictx->z2 * ictx->S2r;
-        double term1 = 1.5 * ictx->ctx->CF * (-2*M_EULER + log(4) - log(ictx->r2 * ictx->ctx->mu2));
-        double term2 = -3.0 * ictx->ctx->CF * (-2*M_EULER + log(4) - log(ictx->r2 * ictx->kT2));
-        double phase1 = ictx->kT * ictx->rx;
-        double phase2 = ictx->kT * ictx->rx / ictx->xi;
+        double amplitude = 0.75*M_1_PI*M_1_PI * ictx->alphasbar * ictx->ctx->CF * ictx->ctx->Sperp / ictx->z2 * ictx->qqfactor * ictx->S2r;
+        double term1 = 0.5 * (-2*M_EULER + log(4) - log(ictx->r2 * ictx->ctx->mu2));
+        double term2 = -(-2*M_EULER + log(4) - log(ictx->r2 * ictx->kT2));
+        double phase1 = -ictx->kT * ictx->rx;
+        double phase2 = -ictx->kT * ictx->rx / ictx->xi;
         *real = amplitude * (term1 * (cos(phase1) + cos(phase2) / ictx->xi2) + term2 * cos(phase1));
         *imag = amplitude * (term1 * (sin(phase1) + sin(phase2) / ictx->xi2) + term2 * sin(phase1));
     }
 };
 
 void I1(double x, double* real, double* imag) {
+    bool negative = false;
     if (x < 0) {
         // if argument is negative, switch it to positive and flip the sign of the imaginary part of the result
         x = -x;
-        *imag = -(2 - cos(x) - gsl_sf_sinc(x * M_1_PI)) / x + 2 * gsl_sf_Si(x);
-    }
-    else {
-        *imag = (2 - cos(x) - gsl_sf_sinc(x * M_1_PI)) / x - 2 * gsl_sf_Si(x);
+        negative = true;
     }
     *real = -2 * M_EULER - gsl_sf_sinc(x * M_1_PI) + (cos(x) - 1) / (x * x) + 2 * gsl_sf_Ci(x) - 2 * log(x);
+    *imag = (negative ? -1 : 1) * ((2 - cos(x) - gsl_sf_sinc(x * M_1_PI)) / x - 2 * gsl_sf_Si(x));
 }
 
 class H14qq : public HardFactor {
@@ -381,8 +380,8 @@ public:
         return quadrupole;
     }
     void Fs(IntegrationContext* ictx, double* real, double* imag) {
-        double amplitude = -1/(4*M_PI*M_PI*4*M_PI*M_PI) * ictx->alphasbar * ictx->qqfactor / ictx->z2 * ictx->S4rst *
-         4*M_PI * ictx->ctx->Nc * (1 + ictx->xi2) / ictx->xi
+        double amplitude = -0.25*M_1_PI*M_1_PI*M_1_PI * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp / ictx->z2 * ictx->qqfactor * ictx->S4rst
+         * (1 + ictx->xi2) / ictx->xi
          * (ictx->sx * ictx->tx + ictx->sy * ictx->ty)
             / ( ictx->s2 * ictx->t2 );
         double phase = -ictx->kT * (ictx->sx / ictx->xi - ictx->tx);
@@ -392,11 +391,12 @@ public:
     void Fd(IntegrationContext* ictx, double* real, double* imag) {
         double realI, imagI;
         I1(ictx->kT * ictx->tx, &realI, &imagI);
-        double amplitude = 1/(4*M_PI*M_PI*4*M_PI*M_PI) * ictx->alphasbar * ictx->qqfactor / ictx->z2 * (ictx->S4rst - ictx->ctx->gdist->S4(ictx->s2, ictx->s2, 0, ictx)) *
-         4*M_PI * ictx->ctx->Nc * ictx->ctx->Sperp / ictx->t2;
-        double phase = ictx->kT * ictx->rx;
-        *real = amplitude * (cos(phase) * realI + sin(phase) * imagI);
-        *imag = amplitude * (cos(phase) * imagI - sin(phase) * realI);
+        double amplitude = 0.25*M_1_PI*M_1_PI*M_1_PI * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp / ictx->z2 * ictx->qqfactor
+          * (ictx->S4rst - ictx->ctx->gdist->S4(ictx->s2, ictx->s2, 0, ictx))
+          / ictx->t2;
+        double phase = -ictx->kT * ictx->rx;
+        *real = amplitude * (cos(phase) * realI - sin(phase) * imagI);
+        *imag = amplitude * (cos(phase) * imagI + sin(phase) * realI);
     }
 };
 
@@ -406,9 +406,9 @@ public:
         return dipole;
     }
     void Fd(IntegrationContext* ictx, double* real, double* imag) {
-        double amplitude = -1/(4*M_PI*M_PI) * ictx->alphasbar * ictx->qqfactor / ictx->z2 * ictx->ctx->gdist->S4(ictx->r2, ictx->r2, 0, ictx) *
-         ictx->ctx->Nc * ictx->ctx->Sperp * (2.5 - 2.0*M_PI*M_PI/3.0);
-        double phase = ictx->kT * ictx->sx;
+        double amplitude = (1.0/6.0 - 0.625*M_1_PI*M_1_PI) * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp / ictx->z2 * ictx->qqfactor
+          * ictx->ctx->gdist->S4(ictx->r2, ictx->r2, 0, ictx);
+        double phase = ictx->kT * ictx->rx;
         *real = amplitude * cos(phase);
         *imag = amplitude * sin(phase);
     }
