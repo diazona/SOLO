027946be7664bb98fd625bfe2a10bf7edd9888d0
David Zaslavsky <diazona@ellipsix.net>
2012-12-10 16:04:24 -0500

    Add momentum update method and appropriate variables to integrationcontext.cpp,h

--- integrationcontext.cpp
+++ integrationcontext.cpp
@@ -7,20 +7,11 @@
 
 #define checkfinite(d) assert(gsl_finite(d))
 
-void IntegrationContext::update(double z, double y, double xx, double xy, double yx, double yy, double bx, double by) {
-    double qqfactor = 0.0d, ggfactor = 0.0d, gqfactor = 0.0d, qgfactor = 0.0d;
-    c_mstwpdf* pdf_object = ctx->pdf_object;
-    DSSpiNLO* ff_object = ctx->ff_object;
+void IntegrationContext::update_parton_factors(double z, double y) {
     assert(z <= 1);
     assert(z >= ctx->tau);
     assert(y <= 1);
     assert(y >= ctx->tau);
-    checkfinite(xx);
-    checkfinite(xy);
-    checkfinite(yx);
-    checkfinite(yy);
-    checkfinite(bx);
-    checkfinite(by);
     this->z = z;
     this->z2 = this->z*this->z;
     if (y == 1.0d) {
@@ -30,21 +21,6 @@ void IntegrationContext::update(double z, double y, double xx, double xy, double
         this->xi = (y * (z - ctx->tau) - ctx->tau * (z - 1)) / (z * (1 - ctx->tau));
     }
     this->xi2 = xi*xi;
-    this->xx = xx;
-    this->xy = xy;
-    this->yx = yx;
-    this->yy = yy;
-    this->bx = bx;
-    this->by = by;
-    this->rx = this->xx - this->yx;
-    this->ry = this->xy - this->yy;
-    this->r2 = rx*rx + ry*ry;
-    this->sx = this->xx - this->bx;
-    this->sy = this->xy - this->by;
-    this->s2 = sx*sx + sy*sy;
-    this->tx = this->yx - this->bx;
-    this->ty = this->yy - this->by;
-    this->t2 = tx*tx + ty*ty;
     this->xp = ctx->tau / (this->z * xi);
     this->kT2 = ctx->pT2 / this->z2;
     this->kT = sqrt(this->kT2);
@@ -52,11 +28,10 @@ void IntegrationContext::update(double z, double y, double xx, double xy, double
     this->Qs2 = ctx->Q02x0lambda / pow(this->xg, ctx->lambda); // Q_0^2 (x_0 / x)^lambda
     this->alphasbar = ctx->cpl->alphasbar(this->kT2);
 
-    // Calculate the new gluon distribution values
-    // this has to be done after kinematics are updated
-    this->S2r = ctx->gdist->S2(r2, this->Qs2);
-    this->S4rst = ctx->gdist->S4(r2, s2, t2, this->Qs2);
-
+    double qqfactor = 0.0d, ggfactor = 0.0d, gqfactor = 0.0d, qgfactor = 0.0d;
+    c_mstwpdf* pdf_object = ctx->pdf_object;
+    DSSpiNLO* ff_object = ctx->ff_object;
+    
     // Calculate the new quark/gluon factors
     pdf_object->update(xp, sqrt(ctx->mu2));
     ff_object->update(z, ctx->mu2);
@@ -114,6 +89,53 @@ void IntegrationContext::update(double z, double y, double xx, double xy, double
     qgfactor *= 2;
     
     this->qgfactor = qgfactor;
+}
 
-    
+void IntegrationContext::update_position(double z, double y, double xx, double xy, double yx, double yy, double bx, double by) {
+    update_parton_factors(z, y);
+    checkfinite(xx);
+    checkfinite(xy);
+    checkfinite(yx);
+    checkfinite(yy);
+    checkfinite(bx);
+    checkfinite(by);
+    this->xx = xx;
+    this->xy = xy;
+    this->yx = yx;
+    this->yy = yy;
+    this->bx = bx;
+    this->by = by;
+    this->rx = this->xx - this->yx;
+    this->ry = this->xy - this->yy;
+    this->r2 = rx*rx + ry*ry;
+    this->sx = this->xx - this->bx;
+    this->sy = this->xy - this->by;
+    this->s2 = sx*sx + sy*sy;
+    this->tx = this->yx - this->bx;
+    this->ty = this->yy - this->by;
+    this->t2 = tx*tx + ty*ty;
+
+    // Calculate the new gluon distribution values
+    // this has to be done after kinematics are updated
+    this->S2r = ctx->gdist->S2(r2, this->Qs2);
+    this->S4rst = ctx->gdist->S4(r2, s2, t2, this->Qs2);
+}
+
+void IntegrationContext::update_momentum(double z, double y, double q1x, double q1y, double q2x, double q2y, double q3x, double q3y) {
+    update_parton_factors(z, y);
+    checkfinite(xx);
+    checkfinite(xy);
+    checkfinite(yx);
+    checkfinite(yy);
+    checkfinite(bx);
+    checkfinite(by);
+    this->q1x = q1x;
+    this->q1y = q1y;
+    this->q2x = q2x;
+    this->q2y = q2y;
+    this->q3x = q3x;
+    this->q3y = q3y;
+    this->q12 = q1x*q1x + q1y*q1y;
+    this->q22 = q2x*q2x + q2y*q2y;
+    this->q32 = q3x*q3x + q3y*q3y;
 }
--- integrationcontext.h
+++ integrationcontext.h
@@ -19,6 +19,9 @@ public:
     double rx, ry, r2;
     double sx, sy, s2;
     double tx, ty, t2;
+    double q1x, q1y, q12;
+    double q2x, q2y, q22;
+    double q3x, q3y, q32;
     double Qs2;
     double alphasbar;
     double qqfactor;
@@ -33,6 +36,9 @@ public:
       xx(0), xy(0),
       yx(0), yy(0),
       bx(0), by(0),
+      q1x(0), q1y(0),
+      q2x(0), q2y(0),
+      q3x(0), q3y(0),
       z2(0), xi2(0),
       kT2(0), kT(0),
       xp(0), xg(0),
@@ -47,7 +53,10 @@ public:
       qgfactor(0),
       S2r(0), S4rst(0) {
     };
-    void update(double z, double y, double xx, double xy, double yx, double yy, double bx, double by);
+    void update_position(double z, double y, double xx, double xy, double yx, double yy, double bx, double by);
+    void update_momentum(double z, double y, double q1x, double q1y, double q2x, double q2y, double q3x, double q3y);
+private:
+    void update_parton_factors(double z, double y);
 };
 
 #endif // _INTEGRATIONCONTEXT_H_
