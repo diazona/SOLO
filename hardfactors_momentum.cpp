de1c41bf7cf981dd0d3d42c5b98576864eef3490
David Zaslavsky <diazona@ellipsix.net>
2012-12-14 12:49:53 -0500

    Fix implementations of H16gg methods in momentum space, and add checks for finiteness of results

--- hardfactors_momentum.cpp
+++ hardfactors_momentum.cpp
@@ -14,29 +14,40 @@ using namespace momentum;
 void H12qqbar::Fd(IntegrationContext* ictx, double* real, double* imag) {
     double value = 2 * ictx->ctx->Nf * ictx->ctx->TR * ictx->ctx->Sperp * ictx->alphasbar * ictx->ggfactor *
         (1 + 2 * ictx->xiprime + 2 * ictx->xiprime2) * ictx->Fq1 * ictx->Fkq1 * log(((ictx->q1x - ictx->xiprime * ictx->kT)*(ictx->q1x - ictx->xiprime * ictx->kT) + ictx->q1y*ictx->q1y)/ictx->kT2);
+    checkfinite(value);
     *real = value;
     *imag = 0;
 }
 
 void H16ggSingular::Fs(IntegrationContext* ictx, double* real, double* imag) {
-    double value = -4 * M_1_PI * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp * ictx->ggfactor / ictx->z2 *
-        (1 - ictx->xi + ictx->xi2)*(1 - ictx->xi + ictx->xi2) / ictx->xi2 *
-        (ictx->q1x * ictx->q2x + ictx->q1y * ictx->q2y) / (ictx->q12 * ictx->q22) *
-        ictx->ctx->gdist->F((ictx->kT + ictx->q1x + ictx->q3x)*(ictx->kT + ictx->q1x + ictx->q3x) + (ictx->q1y + ictx->q3y)*(ictx->q1y + ictx->q3y), ictx->Qs2) *
-        ictx->ctx->gdist->F((ictx->q1x / ictx->xi + ictx->q2x + ictx->q3x)*(ictx->q1x / ictx->xi + ictx->q2x + ictx->q3x) + (ictx->q1y / ictx->xi + ictx->q2y + ictx->q3y)*(ictx->q1y / ictx->xi + ictx->q2y + ictx->q3y), ictx->Qs2) *
-        ictx->Fq3;
+    double Pfac = gsl_pow_2(1 - ictx->xi + ictx->xi2) / ictx->xi2;
+    double dotfac = (ictx->q1x * ictx->q2x + ictx->q1y * ictx->q2y) / (ictx->q12 * ictx->q22);
+    double kA2 = gsl_pow_2(ictx->kT + ictx->q1x + ictx->q3x) + gsl_pow_2(ictx->q1y + ictx->q3y); // (k + q1 + q3)^2
+    double kB2 = gsl_pow_2(ictx->kT / ictx->xi + ictx->q2x + ictx->q3x) + gsl_pow_2(ictx->q2y + ictx->q3y); // (k / ξ + q2 + q3)^2
+    double Fg1 = ictx->ctx->gdist->F(kA2, ictx->Qs2);
+    double Fg2 = ictx->ctx->gdist->F(kB2, ictx->Qs2);
+    double Fg3 = ictx->ctx->gdist->F(ictx->q32, ictx->Qs2);
+    double value = -4 * M_1_PI * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp * ictx->ggfactor / ictx->z2
+         * Pfac * dotfac * Fg1 * Fg2 * Fg3;
+    checkfinite(value);
     *real = value;
     *imag = 0;
 }
 
 void H16ggDelta::Fd(IntegrationContext* ictx, double* real, double* imag) {
-    double value = -4 * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp * ictx->ggfactor / ictx->z2 * (
-        ( ictx->xi * log(((ictx->q2x - ictx->xi * ictx->kT)*(ictx->q2x - ictx->xi * ictx->kT) + ictx->q2y*ictx->q2y) / ictx->kT2) -
-                     log(((ictx->q2x - ictx->kT)*(ictx->q2x - ictx->kT) + ictx->q2y*ictx->q2y) / ictx->kT2) ) / (1 - ictx->xi)
-          + 0.5 * ictx->xi * (1 - ictx->xi) * log(((ictx->q2x - ictx->xi * ictx->kT)*(ictx->q2x - ictx->xi * ictx->kT) + ictx->q2y*ictx->q2y) / ictx->kT2)
-        ) * ictx->Fq1
-          * ictx->ctx->gdist->F((ictx->kT + ictx->q1x)*(ictx->kT + ictx->q1x) + ictx->q1y * ictx->q1y, ictx->Qs2)
-          * ictx->ctx->gdist->F((ictx->q1x + ictx->q2x)*(ictx->q1x + ictx->q2x) + (ictx->q1y + ictx->q2y)*(ictx->q1y + ictx->q2y), ictx->Qs2);
+    double kA02 = gsl_pow_2(ictx->q2x - ictx->xiprime * ictx->kT) + gsl_pow_2(ictx->q2y); // (q2 - xi' k)^2
+    double kA12 = gsl_pow_2(ictx->q2x - ictx->kT) + gsl_pow_2(ictx->q2y); // (q2 - k)^2
+    double kB2 = gsl_pow_2(ictx->q1x + ictx->kT) + gsl_pow_2(ictx->q1y); // (q1 + k)^2
+    double kC2 = gsl_pow_2(ictx->q1x + ictx->q2x) + gsl_pow_2(ictx->q1y + ictx->q2y); // (q1 + q2)^2
+    double Fg1 = ictx->ctx->gdist->F(ictx->q12, ictx->Qs2);
+    double Fg2 = ictx->ctx->gdist->F(kB2, ictx->Qs2);
+    double Fg3 = ictx->ctx->gdist->F(kC2, ictx->Qs2);
+    double log0 = log(kA02 / ictx->kT2);
+    double log1 = log(kA12 / ictx->kT2);
+    double value = -4 * ictx->alphasbar * ictx->ctx->Nc * ictx->ctx->Sperp * ictx->ggfactor / ictx->z2 * Fg1 * Fg2 * Fg3
+                      * ((ictx->xiprime * log0 - log1) / (1 - ictx->xiprime)
+                         + 0.5 * ictx->xiprime * (1 - ictx->xiprime) * log0);
+    checkfinite(value);
     *real = value;
     *imag = 0;
 }
@@ -46,6 +57,7 @@ void H14gq::Fn(IntegrationContext* ictx, double* real, double* imag) {
         ictx->ctx->gdist->F((ictx->kT / ictx->xi + ictx->q1x)*(ictx->kT / ictx->xi + ictx->q1x) + ictx->q1y*ictx->q1y, ictx->Qs2) *
         ictx->ctx->gdist->F((ictx->kT * (1 - 1. / ictx->xi) - ictx->q1x + ictx->q2x)*(ictx->kT * (1 - 1. / ictx->xi) - ictx->q1x + ictx->q2x) + (-ictx->q1y + ictx->q2y)*(-ictx->q1y + ictx->q2y), ictx->Qs2) *
         (ictx->q1x * ictx->q2x + ictx->q1y * ictx->q2y) / (ictx->q12 * ictx->q22);
+    checkfinite(value);
     *real = value;
     *imag = 0;
 }
@@ -55,6 +67,7 @@ void H14qg::Fn(IntegrationContext* ictx, double* real, double* imag) {
         ictx->ctx->gdist->F((ictx->kT - ictx->q1x)*(ictx->kT - ictx->q1x) + ictx->q1y*ictx->q1y, ictx->Qs2) *
         ictx->ctx->gdist->F((ictx->kT * (1 - 1. / ictx->xi) - ictx->q1x + ictx->q2x)*(ictx->kT * (1 - 1. / ictx->xi) - ictx->q1x + ictx->q2x) + (-ictx->q1y + ictx->q2y)*(-ictx->q1y + ictx->q2y), ictx->Qs2) *
         (ictx->q1x * ictx->q2x + ictx->q1y * ictx->q2y) / (ictx->q12 * ictx->q22);
+    checkfinite(value);
     *real = value;
     *imag = 0;
 }
